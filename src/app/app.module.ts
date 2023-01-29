a9aadd871337180f6c4ff1171bd37731ceaf8384
alejandro.perez.acosta@hotmail.com <alejandro.perez.acosta@hotmail.com>
2023-01-28 19:36:56 -0500

    [SPAE CONSULTA LOG] 1) Botón de Búsqueda

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule     } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
-import { MatListModule           } from '@angular/material/list';
-import { MatTableModule          } from '@angular/material/table';
-import { MatPaginatorModule      } from '@angular/material/paginator';
-import { AppComponent } from './app.component';
-import { LogInfoViewComponent } from './loginfoview/loginfoview.component';
+import { NgModule                 } from '@angular/core';
+import { BrowserModule            } from '@angular/platform-browser';
+import { BrowserAnimationsModule  } from '@angular/platform-browser/animations';
+import { HttpClientModule         } from '@angular/common/http';
+import { AppRoutingModule         } from './app-routing.module';
+import { MatListModule            } from '@angular/material/list';
+import { MatTableModule           } from '@angular/material/table';
+import { MatPaginatorModule       } from '@angular/material/paginator';
+import { AppComponent             } from './app.component';
+import { LogInfoViewComponent     } from './loginfoview/loginfoview.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { LogInfoViewComponent } from './loginfoview/loginfoview.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
     MatListModule,
