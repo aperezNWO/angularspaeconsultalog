21deda192d12ab0f64160723c134a9ae681f05aa
alejandro.perez.acosta@hotmail.com <alejandro.perez.acosta@hotmail.com>
2023-03-13 09:50:20 -0500

    Aplicativo : [SPAE CONSULTAS] Version :1.0.0.45 1) Consulta Log. a) Panel de Estadisticas.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@
  export class AppComponent {
    //
    appName           : string = "[SPAE CONSULTAS]"
-   appVersion        : string = '1.0.0.44';
+   appVersion        : string = '1.0.0.45';
    runtimeVersion    : string = VERSION.full;
    //
    readonly LogInfoViewComponent_pageTitle             : string = LogInfoViewComponent.pageTitle();
